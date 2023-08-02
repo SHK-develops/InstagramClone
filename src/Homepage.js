cf8896e6ba14b4da6bd219e17ebc549b1dd3f19b
HEMANTH KAMAL SEVA <52732015+SHK-develops@users.noreply.github.com>
2023-08-02 17:46:45 +1000

    basic

---
+++ src/Homepage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Homepage() {
+  return (
+    <div className='homepage'>
+        <div className='homepage__nav'>
+    </div>
+    <div className='homepage__timeline'>
+    </div>
+    </div>
+  )
+}
+
+export default Homepage
